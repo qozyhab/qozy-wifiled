a636398b6714486802601f6bc081180faa5d09a9
Kevin <kevin@qozy.io>
2019-05-13 10:14:35 +0200

    compatibility to current qozy version

--- qozy_wifiled/bridge.py
+++ qozy_wifiled/bridge.py
@@ -1,46 +1,60 @@
+import asyncio
+
 from flux_led import BulbScanner, WifiLedBulb
+import logging
 
 from qozyd.models.bridges.exceptions import OfflineException
-from qozyd.models.bridges import Bridge
 from qozyd.models.channels import SwitchChannel, ColorChannel, NumberChannel
 from qozyd.models.things import Thing
 
-import time
+
+from qozyd.plugins.bridge import BridgePlugin
+from qozyd.utils import as_coroutine
 from qozyd.utils.color import RGB
 
 
-class WifiLED(Bridge):
+logger = logging.getLogger(__name__)
+
+
+class WifiLED(BridgePlugin):
     VENDOR_PREFIX = "wifiled"
-    _v_bulbs = {}
-    _v_scanner = BulbScanner()
 
-    def __init__(self, instance_id):
-        super().__init__(instance_id)
+    def __init__(self, bridge):
+        super().__init__(bridge)
 
-        self._v_scanner = BulbScanner()
-        self._v_bulbs = {}
+        self.scanner = BulbScanner()
+        self.bulbs = {}
 
     @property
     def active(self):
         return True
 
-    def start(self, connection):
+    @as_coroutine
+    def _scan(self):
+        return self.scanner.scan()
+
+    async def find(self):
+        bulb_infos = await self._scan()
+
+        self.bulbs.clear()
+        for bulb_info in bulb_infos:
+            self.bulbs[bulb_info["id"]] = WifiLedBulb(bulb_info["ipaddr"])
+
+    async def start(self, connection):
+        await self.find()
+
         while not self.stopped:
-            bulb_infos = self._v_scanner.scan()
-
-            self._v_bulbs.clear()
-            for bulb_info in bulb_infos:
-                self._v_bulbs[bulb_info["id"]] = WifiLedBulb(bulb_info["ipaddr"])
-
             for thing in self.things.values():
                 if self.is_online(thing):
-                    self.update_state(thing)
+                    await self.update_state(thing)
 
-            time.sleep(1)
+            await asyncio.sleep(1)
 
-    def scan(self):
-        for bulb in self._v_scanner.found_bulbs:
-            thing = Thing(self, bulb["id"])
+    async def scan(self):
+        await self.find()
+
+        for bulb in self.scanner.found_bulbs:
+            thing = Thing(self.bridge, bulb["id"])
             thing.add_channel(SwitchChannel(thing, "power"))
             thing.add_channel(ColorChannel(thing, "color"))
             thing.add_channel(NumberChannel(thing, "coldwhite", min=0, max=100))
@@ -49,22 +63,22 @@ class WifiLED(Bridge):
             yield thing
 
     def is_online(self, thing):
-        return thing.local_id in self._v_bulbs
+        return thing.local_id in self.bulbs
 
-    def update_state(self, thing):
-        bulb = self._v_bulbs[thing.local_id]
+    async def update_state(self, thing):
+        bulb = self.bulbs[thing.local_id]
         bulb.update_state()
 
-        thing.channel("power").set(bulb.isOn())
-        thing.channel("color").set(RGB(*bulb.getRgb()))
-        thing.channel("coldwhite").set(int(bulb.cold_white / 255 * 100))
-        thing.channel("warmwhite").set(int(bulb.warm_white / 255 * 100))
+        await thing.channel("power").set(bulb.isOn())
+        await thing.channel("color").set(RGB(*bulb.getRgb()))
+        await thing.channel("coldwhite").set(int(bulb.cold_white / 255 * 100))
+        await thing.channel("warmwhite").set(int(bulb.warm_white / 255 * 100))
 
-    def apply(self, thing, channel, value):
+    async def apply(self, thing, channel, value):
         if not self.is_online(thing):
             raise OfflineException
 
-        bulb = self._v_bulbs[thing.local_id]
+        bulb = self.bulbs[thing.local_id]
 
         if channel.name == "power":
             if value:
@@ -72,12 +86,12 @@ class WifiLED(Bridge):
             else:
                 bulb.turnOff()
         elif channel.name == "color":
-            bulb.setRgb(value[0], value[1], value[2])
+            bulb.setRgb(*value.rgb())
         elif channel.name == "coldwhite":
             bulb.setColdWhite(value)
         elif channel.name == "warmwhite":
             bulb.setWarmWhite(value)
         
-        channel.set(value)
+        await channel.set(value)
 
         return True
